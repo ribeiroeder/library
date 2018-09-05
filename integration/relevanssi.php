c353c374441d8ae10480725d5b1387be3fd868c6
ribeiroeder <contato@ederribeiro.net>
2018-09-05 13:07:43 -0300

    Adding Comments

--- integration/relevanssi.php
+++ integration/relevanssi.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Relevanssi index attachment Metabox.io
+ * Expand searches of the Relevanssi plugin to find the attachments name of the Metabox 'File Advanced" field associated with the post parent.
+ * 
+ */
+
 add_filter( 'relevanssi_content_to_index', 'rlv_index_attachment_names', 10, 2 );
 function rlv_index_attachment_names( $content, $post_id ) {
 	if ($post_id->post_type == "your_post_type") {
